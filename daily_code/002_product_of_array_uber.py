6dd24d2d13fef0b48615285f58b2ba517b580137
dineshh <dinesh.kumar@avmsolutionsuk.com>
2022-03-15 11:00:16 +0000

    added-product

--- daily_code/002_product_of_array_uber.py
+++ daily_code/002_product_of_array_uber.py
@@ -10,3 +10,64 @@ input: [3, 2, 1]
 output: [2, 3, 6]
 
 """
+# Method 1
+def product_of_array(list_of_integers):
+
+    output = []
+    
+    # convert list into iterator and get the index and its value
+    for idx, val in enumerate(list_of_integers):
+
+        list_of_integers[idx] = 1 # chage index into 1
+
+        result = 1 # TO store multiple value
+
+
+        for i in list_of_integers: # loop through the list
+            result *= i # multiply
+
+        output.append(result) # push into new list
+
+        list_of_integers[idx] = val 
+
+    return output
+
+# Method 2
+def product_of_array_2(list_of_integers):
+    
+    output = [] # to store output value
+    prod = 1 # To store product value
+
+    for i in list_of_integers:
+        prod *= i # Get Product
+    
+    for i in list_of_integers:
+        output.append(prod//i) # divid each value with product
+
+    return output
+
+
+# Method 3
+def product_of_array_3(list_of_integers):
+
+    # Intialize output array
+    output = [1]*len(list_of_integers)
+
+    pre_prod = suf_prod = 1
+    # Get product of values before index
+    for i in range(1,len(list_of_integers)):
+        pre_prod *= list_of_integers[i-1]
+        output[i] = pre_prod
+    
+    # Get product of values after index
+    for i in range(len(list_of_integers)-2,-1,-1):
+        suf_prod *= list_of_integers[i+1]
+        
+        output[i] *= suf_prod
+
+    return output
+
+# Execution 
+list_1 = [20, 2, 3, 4, 5]
+
+print(product_of_array_3(list_1))
---
+++ daily_code/174_type_of_polymorphism_microsoft.py
@@ -0,0 +1,32 @@
+"""
+
+Describe and give an example of each of the following types of polymorphism
+
+* Ad-hoc Polymorphism
+* Parametric Polymorphism
+* Subtype Polymorphism 
+
+Ad-hoc Polymorphism:
+
+    We can implement overload function based on different types.
+    EX: + behaves differently based on data type. (integer, float, list, string concat)
+
+Parametric Polymorphism:
+
+    Generic implementation independent of types. 
+
+    ex: Append 
+
+Subtype Polymorphism:
+
+    When we subclass a class and use each subclass method to implement a specific interface.
+
+    Class shape:
+        def draw()
+
+    class circle(shape)
+        def draw()
+
+
+
+"""
