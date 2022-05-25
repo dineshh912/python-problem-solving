307a05704ef0621a4d28eddaade43d47449c7a79
dineshh912 <dineshh912@gmail.com>
2022-05-25 15:57:48 +0100

    frq_queries_added

--- all_programs/hacker_rank/dict_has_maps/04_count_triplets.py
+++ all_programs/hacker_rank/dict_has_maps/04_count_triplets.py
@@ -7,6 +7,35 @@ example :
 
 arr = [1, 4, 16, 64] r = 4
 
-
+possilble triplets and it's indices: 
+[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3], []
 
 """
+def countTriplets(arr, r):
+    count = 0
+    before = {}
+    after = {}
+
+    for i in arr:
+        if i in after:
+            after[i] += 1
+        else:
+            after[i] = 1
+
+    for i in arr:
+        after[i] -= 1 # current element
+        if i//r in before and i % r == 0 and i * r in after:
+            count += before[i//r] * after[i*r]
+        
+        if i in before:
+            before[i] += 1
+        else:
+            before[i] = 1
+    
+    return count
+    
+
+
+
+
+print(countTriplets([2, 4, 16, 64], 4))
--- all_programs/hacker_rank/dict_has_maps/05_freq_queries.py
+++ all_programs/hacker_rank/dict_has_maps/05_freq_queries.py
@@ -3,5 +3,19 @@ you are given q queries. Each query is of the form two integers
 
 x: insert x in ds
 y: delete one occourece 
+z : Check if any integer is present whose frequency is exactly . If yes, print 1 else 0.
+
+8
+1 5
+1 6
+3 2
+1 10
+1 10
+1 6
+2 5
+3 2
+
+0
+1
 
 """
